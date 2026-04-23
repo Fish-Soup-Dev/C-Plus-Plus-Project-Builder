33acb3719d7d6c06e91a3ad9c40f3f6f014bb1f0
Fish-Soup-Dev <andrewkawalec164@gmail.com>
2026-04-23 12:58:23 -0400

    Add class header and cpp templates
    Introduce template helpers for generating C++ class boilerplate.
    New files: src/templates/classTemplate.cpp and classTemplate.hpp.
    Expose two functions: classHeaderTemplate and classTemplate.
    Header declares prototypes; cpp provides implementations.

---
+++ src/templates/classTemplate.cpp
@@ -0,0 +1,47 @@
+#include "classTemplate.hpp"
+
+std::string classHeaderTemplate(const std::string classNameUpper, const std::string className)
+{
+    return
+R"(#ifndef )" + classNameUpper + R"(_HPP
+#define )" + classNameUpper + R"(_HPP
+
+class )" + className + R"(
+{
+public:
+    )" + className + R"(()
+    {
+
+    }
+
+    ~)" + className + R"(()
+    {
+
+    }
+
+    // more stuff here
+
+private:
+    // stuff go here
+
+}
+#endif)";
+}
+
+std::string classTemplate(const std::string className)
+{
+    return
+R"(#include ")" + className + R"(.h"
+
+)" + className + R"(::)" + className + R"(()
+{
+
+}
+
+)" + className + R"(::~)" + className + R"(()
+{
+
+}
+
+)";
+}
---
+++ src/templates/classTemplate.hpp
@@ -0,0 +1,9 @@
+#ifndef CLASS_TEMPLATE_HPP
+#define CLASS_TEMPLATE_HPP
+
+#include <string>
+
+std::string classHeaderTemplate(const std::string classNameUpper, const std::string className);
+std::string classTemplate(const std::string name);
+
+#endif
